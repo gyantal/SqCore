02981862082931c0cf1382dfd62e5af24e55e890
Dr. George Antal <gyantal1@gmail.com>
2020-05-14 18:45:56 +0100

    class AppSettings to group app parameters in one place

--- src/WebServer/SqCoreWeb/projects/MarketDashboard/src/app/settings-dialog/settings-dialog.component.ts
+++ src/WebServer/SqCoreWeb/projects/MarketDashboard/src/app/settings-dialog/settings-dialog.component.ts
@@ -1,5 +1,12 @@
 import { Component, Output, EventEmitter } from '@angular/core';
 
+export class AppSettings {  // collect the settings variables here. This will be stored in a database in server code.
+  uiTheme = '';
+  marketHealthDefaultLookback = 'YTD';  // example. Not used yet.
+  marketHealthDefaultStatistics = 'Return'; // example. Not used yet.
+  catalystSnifferShowDetailedTooltips = true; // example. Not used yet.
+}
+
 @Component({
   selector: 'settings-dialog',
   templateUrl: './settings-dialog.component.html',
@@ -9,9 +16,12 @@ export class SettingsDialogComponent {
   isVisible = false;  // see https://stackoverflow.com/questions/59013913/how-to-manipulate-a-div-style-in-angular-8
   @Output() parentChangeThemeEvent = new EventEmitter<string>();
 
+  _appSettings = new AppSettings();
+
   constructor() { }
 
   onSetThemeSelector(theme: string) {
+    this._appSettings.uiTheme = theme;
     console.log(theme);
     this.parentChangeThemeEvent.emit(theme);
   }
