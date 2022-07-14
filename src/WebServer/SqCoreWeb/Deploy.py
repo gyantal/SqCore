6dde37f5b7a46c8688eccede3779099efc225bb0
DayakarKodirekka <dayakar.kodirekka@gmail.com>
2022-07-14 18:05:52 +0530

    adding Daya's Desktop credintails

--- src/WebServer/SqCoreWeb/Deploy.py
+++ src/WebServer/SqCoreWeb/Deploy.py
@@ -31,6 +31,9 @@ elif runningEnvironmentComputerName == 'gyantal-SfBook3':
 elif runningEnvironmentComputerName == 'daya-PC':
     rootLocalDir = "c:/GitHubRepos/SqCore/src/WebServer/SqCoreWeb/bin/Release/net6.0/publish"       #os.walk() gives back in a way that the last character is not slash, so do that way
     serverRsaKeyFile = "g:/.shortcut-targets-by-id/0BzxkV1ug5ZxvVmtic1FsNTM5bHM/GDriveHedgeQuant/shared/GitHubRepos/NonCommitedSensitiveData/cert/AwsSqCore/AwsSqCore,sq-vnc-client.pem"
+elif runningEnvironmentComputerName == 'DAYA-DESKTOP':
+    rootLocalDir = "c:/GitHubRepos/SqCore/src/WebServer/SqCoreWeb/bin/Release/net6.0/publish"       #os.walk() gives back in a way that the last character is not slash, so do that way
+    serverRsaKeyFile = "g:/.shortcut-targets-by-id/0BzxkV1ug5ZxvVmtic1FsNTM5bHM/GDriveHedgeQuant/shared/GitHubRepos/NonCommitedSensitiveData/cert/AwsSqCore/AwsSqCore,sq-vnc-client.pem"
 else:   # TODO: Laci, Balazs, you have to add your IF here (based on the 'name' of your PC)
     rootLocalDir = "d:/GitHub/SqCore/src/WebServer/SqCoreWeb/bin/Release/net6.0/publish"       #os.walk() gives back in a way that the last character is not slash, so do that way
     serverRsaKeyFile = "d:/<fill it to user>/Google Drive/GDriveHedgeQuant/shared/GitHubRepos/NonCommitedSensitiveData/cert/AwsSqCore/AwsSqCore,sq-vnc-client.pem"
