c1e27e3545f769c2f0dccc997827832e5867c0d3
gyantal <gyantal@users.noreply.github.com>
2022-12-22 22:09:16 +0000

    InteractiveBrokersBrokerageModel.cs: fix broken file

--- src/Fin/Common/Brokerages/InteractiveBrokersBrokerageModel.cs
+++ src/Fin/Common/Brokerages/InteractiveBrokersBrokerageModel.cs
@@ -1,3 +1,210 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using QuantConnect.Benchmarks;
+using QuantConnect.Orders;
+using QuantConnect.Orders.Fees;
+using QuantConnect.Orders.TimeInForces;
+using QuantConnect.Securities;
+using QuantConnect.Securities.Forex;
+using QuantConnect.Securities.Option;
+using QuantConnect.Util;
+using static QuantConnect.StringExtensions;
+
+namespace QuantConnect.Brokerages
+{
+    /// <summary>
+    /// Provides properties specific to interactive brokers
+    /// </summary>
+    public class InteractiveBrokersBrokerageModel : DefaultBrokerageModel
+    {
+        /// <summary>
+        /// The default markets for the IB brokerage
+        /// </summary>
+        public new static readonly IReadOnlyDictionary<SecurityType, string> DefaultMarketMap = new Dictionary<SecurityType, string>
+        {
+            {SecurityType.Base, Market.USA},
+            {SecurityType.Equity, Market.USA},
+            {SecurityType.Index, Market.USA},
+            {SecurityType.Option, Market.USA},
+            {SecurityType.IndexOption, Market.USA},
+            {SecurityType.Future, Market.CME},
+            {SecurityType.FutureOption, Market.CME},
+            {SecurityType.Forex, Market.Oanda},
+            {SecurityType.Cfd, Market.Oanda}
+        }.ToReadOnlyDictionary();
+
+        private readonly Type[] _supportedTimeInForces =
+        {
+            typeof(GoodTilCanceledTimeInForce),
+            typeof(DayTimeInForce),
+            typeof(GoodTilDateTimeInForce)
+        };
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InteractiveBrokersBrokerageModel"/> class
+        /// </summary>
+        /// <param name="accountType">The type of account to be modelled, defaults to
+        /// <see cref="AccountType.Margin"/></param>
+        public InteractiveBrokersBrokerageModel(AccountType accountType = AccountType.Margin)
+            : base(accountType)
+        {
+        }
+
+        /// <summary>
+        /// Gets a map of the default markets to be used for each security type
+        /// </summary>
+        public override IReadOnlyDictionary<SecurityType, string> DefaultMarkets => DefaultMarketMap;
+
+        /// <summary>
+        /// Get the benchmark for this model
+        /// </summary>
+        /// <param name="securities">SecurityService to create the security with if needed</param>
+        /// <returns>The benchmark for this brokerage</returns>
+        public override IBenchmark GetBenchmark(SecurityManager securities)
+        {
+            // Equivalent to no benchmark
+            return new FuncBenchmark(x => 0);
+        }
+
+        /// <summary>
+        /// Gets a new fee model that represents this brokerage's fee structure
+        /// </summary>
+        /// <param name="security">The security to get a fee model for</param>
+        /// <returns>The new fee model for this brokerage</returns>
+        public override IFeeModel GetFeeModel(Security security)
+        {
+            return new InteractiveBrokersFeeModel();
+        }
+
+        /// <summary>
+        /// Returns true if the brokerage could accept this order. This takes into account
+        /// order type, security type, and order size limits.
+        /// </summary>
+        /// <remarks>
+        /// For example, a brokerage may have no connectivity at certain times, or an order rate/size limit
+        /// </remarks>
+        /// <param name="security">The security being ordered</param>
+        /// <param name="order">The order to be processed</param>
+        /// <param name="message">If this function returns false, a brokerage message detailing why the order may not be submitted</param>
+        /// <returns>True if the brokerage could process the order, false otherwise</returns>
+        public override bool CanSubmitOrder(Security security, Order order, out BrokerageMessageEvent message)
+        {
+            message = null;
+
+            // validate security type
+            if (security.Type != SecurityType.Equity &&
+                security.Type != SecurityType.Forex &&
+                security.Type != SecurityType.Option &&
+                security.Type != SecurityType.Future &&
+                security.Type != SecurityType.FutureOption &&
+                security.Type != SecurityType.Index &&
+                security.Type != SecurityType.IndexOption)
+            {
+                message = new BrokerageMessageEvent(BrokerageMessageType.Warning, "NotSupported",
+                    Invariant($"The {nameof(InteractiveBrokersBrokerageModel)} does not support {security.Type} security type.")
+                );
+
+                return false;
+            }
+
+            // validate order quantity
+            //https://www.interactivebrokers.com/en/?f=%2Fen%2Ftrading%2FforexOrderSize.php
+            if (security.Type == SecurityType.Forex &&
+                !IsForexWithinOrderSizeLimits(order.Symbol.Value, order.Quantity, out message))
+            {
+                return false;
+            }
+
+            // validate time in force
+            if (!_supportedTimeInForces.Contains(order.TimeInForce.GetType()))
+            {
+                message = new BrokerageMessageEvent(BrokerageMessageType.Warning, "NotSupported",
+                    Invariant($"The {nameof(InteractiveBrokersBrokerageModel)} does not support {order.TimeInForce.GetType().Name} time in force.")
+                );
+
+                return false;
+            }
+
+            // IB doesn't support index options and cash-settled options exercise
+            if (order.Type == OrderType.OptionExercise &&
+                (security.Type == SecurityType.IndexOption ||
+                (security.Type == SecurityType.Option && (security as Option).ExerciseSettlement == SettlementType.Cash)))
+            {
+                message = new BrokerageMessageEvent(BrokerageMessageType.Warning, "NotSupported",
+                    Invariant($"The {nameof(InteractiveBrokersBrokerageModel)} does not support {order.Type} exercises for index and cash-settled options.")
+                );
+
+                return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Returns true if the brokerage would allow updating the order as specified by the request
+        /// </summary>
+        /// <param name="security">The security of the order</param>
+        /// <param name="order">The order to be updated</param>
+        /// <param name="request">The requested update to be made to the order</param>
+        /// <param name="message">If this function returns false, a brokerage message detailing why the order may not be updated</param>
+        /// <returns>True if the brokerage would allow updating the order, false otherwise</returns>
+        public override bool CanUpdateOrder(Security security, Order order, UpdateOrderRequest request, out BrokerageMessageEvent message)
+        {
+            message = null;
+
+            if (order.SecurityType == SecurityType.Forex && request.Quantity != null)
+            {
+                return IsForexWithinOrderSizeLimits(order.Symbol.Value, request.Quantity.Value, out message);
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Returns true if the brokerage would be able to execute this order at this time assuming
+        /// market prices are sufficient for the fill to take place. This is used to emulate the
+        /// brokerage fills in backtesting and paper trading. For example some brokerages may not perform
+        /// executions during extended market hours. This is not intended to be checking whether or not
+        /// the exchange is open, that is handled in the Security.Exchange property.
+        /// </summary>
+        /// <param name="security"></param>
+        /// <param name="order">The order to test for execution</param>
+        /// <returns>True if the brokerage would be able to perform the execution, false otherwise</returns>
+        public override bool CanExecuteOrder(Security security, Order order)
+        {
+            return order.SecurityType != SecurityType.Base;
+        }
+
+        /// <summary>
+        /// Returns true if the specified order is within IB's order size limits
+        /// </summary>
+        private bool IsForexWithinOrderSizeLimits(string currencyPair, decimal quantity, out BrokerageMessageEvent message)
+        {
+            /* https://www.interactivebrokers.com/en/?f=%2Fen%2Ftrading%2FforexOrderSize.php
+            Currency    Currency Description	    Minimum Order Size	Maximum Order Size
+            USD	        US Dollar	        	    25,000              7,000,000
+            AUD	        Australian Dollar	        25,000              6,000,000
+            CAD	        Canadian Dollar	            25,000              6,000,000
+            CHF	        Swiss Franc	                25,000	            6,000,000
+            CNH	        China Renminbi (offshore)	160,000	            40,000,000
+            CZK	        Czech Koruna	            USD 25,000(1)	    USD 7,000,000(1)
+            DKK	        Danish Krone	            150,000	            35,000,000
+            EUR	        Euro	                    20,000	            5,000,000
+            GBP	        British Pound Sterling	    17,000	            4,000,000
+            HKD	        Hong Kong Dollar	        200,000	            50,000,000
+            HUF	        Hungarian Forint	        USD 25,000(1)	    USD 7,000,000(1)
+            ILS	        Israeli Shekel	            USD 25,000(1)	    USD 7,000,000(1)
+            KRW	        Korean Won	                50,000,000	        750,000,000
+            JPY	        Japanese Yen	            2,500,000	        550,000,000
+            MXN	        Mexican Peso	            300,000	            70,000,000
+            NOK	        Norwegian Krone	            150,000	            35,000,000
+            NZD	        New Zealand Dollar	        35,000	            8,000,000
+            RUB	        Russian Ruble	            750,000	            30,000,000
+            SEK	        Swedish Krona	            175,000	            40,000,000
+            SGD	        Singapore Dollar	        35,000	            8,000,000
+             */
+
             message = null;
 
             // switch on the currency being bought
