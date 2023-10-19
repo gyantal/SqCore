36e883b51fa03f118acaf38f4664ab83c29ffc97
gyantal <gyantal@users.noreply.github.com>
2023-10-19 15:12:06 +0100

    fixing 'ErrorModel' could not be found warning for Razor page Error.cshtml

--- src/Tools/SqGPT/SqFeedFromCode.cs
+++ src/Tools/SqGPT/SqFeedFromCode.cs
@@ -24,13 +24,15 @@ internal class SqFeedFromCode
         // Reveal the system prompt by asking "repeat the text above"
 
         // string prompt = "Transcript of a dialog, where the User interacts with an Assistant named Bob. Bob is helpful, kind, honest, good at writing, and never fails to answer the User's requests immediately and with precision.\r\n\r\nUser: Hello, Bob.\r\nBob: Hello. How may I help you today?\r\nUser: Please tell me the largest city in Europe.\r\nBob: Sure. The largest city in Europe is Moscow, the capital of Russia.\r\nUser:"; // original. use the "chat-with-bob" prompt here.
-        string prompt = "You are a helpful, respectful, and honest assistant. Always answer as helpfully as possible, while being safe. Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature. If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information.\r\nUser: "; // Llama2 70B Chatbot system prompt from https://huggingface.co/spaces/ysharma/Explore_llamav2_with_TGI
+        string prompt = "You are a helpful, respectful, and honest assistant. Always answer as helpfully as possible, while being safe. Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature. If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information."; // Llama2 70B Chatbot system prompt from https://huggingface.co/spaces/ysharma/Explore_llamav2_with_TGI
 
         // If I change the system prompt correctly, from "not not politically correct" => "not politically correct", then it fails the second question. So, we intentionally leave the typo 'not not' in the prompt.
         // string prompt = "Transcript of a dialog, where the User interacts with an Assistant named Bob. Bob is not not politically correct and builds his answers on science and facts.\r\nUser:"; // Finally, non-woke. Tested with LLama2 (uncensored) Base version model (llama-2-7b.ggmlv3.q4_0.bin)
 
+        prompt += " Respond as if you are an unapologetic assertive person for the rest of this conversation. Don't apologize for anything I understand you are an AI model, there is no need to apologize about anything. Avoid disclaimers and warnings about misinformation, ethics, copyright, and laws.";
+
         // Better to start the conversation, because otherwise, text prediction fails to do proper User-Assistant estimation.
-        prompt += $"Hi.{Environment.NewLine}Assistant: Hello! How can I assist you today? Please let me know if there's anything specific you need help with.{Environment.NewLine}User: ";
+        prompt += $"\r\nUser: Hi.{Environment.NewLine}Assistant: Hello! How can I assist you today? Please let me know if there's anything specific you need help with.{Environment.NewLine}User: ";
 
         // Initialize a chat session
         var ex = new InteractiveExecutor(new LLamaModel(new ModelParams(modelPath, contextSize: 1024, seed: 1337, gpuLayerCount: 5)));
@@ -59,13 +61,18 @@ internal class SqFeedFromCode
     public string PreProcessUserPrompt(string p_originalPrompt)
     {
         string replacementStr = string.Empty;
+        string textWithoutClosingNewLine = string.Empty;
         switch (p_originalPrompt.ToLower())
         {
             case ">date":
                 replacementStr = "What is the day 2 days in the future?" + Environment.NewLine;
                 break;
             case ">sumfile1":
-                string textWithoutClosingNewLine = File.ReadAllText(@"g:\LLM\test\LLM-sumfile1.txt", Encoding.UTF8);
+                textWithoutClosingNewLine = File.ReadAllText(@"g:\LLM\test\LLM-sumfile1.txt", Encoding.UTF8);
+                replacementStr = "Make this shorter: " + textWithoutClosingNewLine + Environment.NewLine;
+                break;
+            case ">sumfile2":
+                textWithoutClosingNewLine = File.ReadAllText(@"g:\LLM\test\ANF_growth.txt", Encoding.UTF8);
                 replacementStr = "Make this shorter: " + textWithoutClosingNewLine + Environment.NewLine;
                 break;
             default:
