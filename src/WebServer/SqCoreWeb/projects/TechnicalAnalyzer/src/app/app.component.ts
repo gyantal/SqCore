02e6da0bd0cd68b8ff420cf42911545e1c33daa7
DayakarKodirekka <dayakar.kodirekka@gmail.com>
2024-05-31 09:23:14 +0530

    TechAnlyzr : pctChnData

--- src/WebServer/SqCoreWeb/projects/TechnicalAnalyzer/src/app/app.component.ts
+++ src/WebServer/SqCoreWeb/projects/TechnicalAnalyzer/src/app/app.component.ts
@@ -1,9 +1,8 @@
 import { Component, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-class Asset {
-  // public sqTicker = ''; // “S/TSLA” // sqTicker identifies the asset uniquely in C#, but we don’t need that at the moment. Here we use only the Symbol.
-  public symbol = ''; // “TSLA”
+class PctChnData {
+  public Date: Date = new Date();
   public pctChnSignal1: number = 0; // 0% or 100%
   public pctChnSignal2: number = 0;
   public pctChnSignal3: number = 0;
@@ -11,9 +10,10 @@ class Asset {
   public pctChnWeightAggregate: number = 0; // 0% or 25% 50% 75% 100%
 }
 
-class HistData {
-  Date: Date = new Date();
-  Assets: Asset[] = [];
+class AssetHistData {
+  // public sqTicker = ''; // “S/TSLA” // sqTicker identifies the asset uniquely in C#, but we don’t need that at the moment. Here we use only the Symbol.
+  public symbol = ''; // “TSLA”
+  public pctChnDatas: PctChnData[] = [];
 }
 
 // SnapshotData; contain only the latest values of that Technical factor. E.g. SMA50, SMA200.  m_snapDatas;
@@ -28,25 +28,7 @@ export class AppComponent {
   m_httpClient: HttpClient;
   m_controllerBaseUrl: string;
   m_tickersStr: string | null = null;
-  m_histDatas: HistData[] = [
-    {
-      'Date': new Date('2024-05-10'),
-      'Assets': [
-        {'symbol': 'TSLA', 'pctChnSignal1': 0, 'pctChnSignal2': 0.25, 'pctChnSignal3': 0.55, 'pctChnSignal4': 0.11, 'pctChnWeightAggregate': 0.91 },
-        {'symbol': 'MSFT', 'pctChnSignal1': 0.1, 'pctChnSignal2': 0.22, 'pctChnSignal3': 0.45, 'pctChnSignal4': 0.65, 'pctChnWeightAggregate': 1.42 },
-        {'symbol': 'TLT', 'pctChnSignal1': 0, 'pctChnSignal2': 0.25, 'pctChnSignal3': 0.55, 'pctChnSignal4': 0.11, 'pctChnWeightAggregate': 0.91 },
-        {'symbol': 'SPY', 'pctChnSignal1': 0, 'pctChnSignal2': 0.25, 'pctChnSignal3': 0.55, 'pctChnSignal4': 0.1, 'pctChnWeightAggregate': 0.81 }
-      ]
-    },
-    {
-      'Date': new Date('2024-05-9'),
-      'Assets': [
-        {'symbol': 'TSLA', 'pctChnSignal1': 0.05, 'pctChnSignal2': 0.28, 'pctChnSignal3': 0.52, 'pctChnSignal4': 0.09, 'pctChnWeightAggregate': 0.94 },
-        {'symbol': 'MSFT', 'pctChnSignal1': 0.12, 'pctChnSignal2': 0.21, 'pctChnSignal3': 0.41, 'pctChnSignal4': 0.68, 'pctChnWeightAggregate': 1.42 },
-        {'symbol': 'TLT', 'pctChnSignal1': 0.03, 'pctChnSignal2': 0.22, 'pctChnSignal3': 0.58, 'pctChnSignal4': 0.1, 'pctChnWeightAggregate': 0.93 },
-        {'symbol': 'SPY', 'pctChnSignal1': 0.01, 'pctChnSignal2': 0.24, 'pctChnSignal3': 0.57, 'pctChnSignal4': 0.12, 'pctChnWeightAggregate': 0.94 }
-      ]
-    }]; // dummy data
+  m_assetHistDatas: AssetHistData[] = [];
 
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
     this.m_httpClient = http;
@@ -55,7 +37,7 @@ export class AppComponent {
 
     const url = new URL(window.location.href); // https://sqcore.net/webapps/TechnicalAnalyzer/?tickers=TSLA,MSFT
     this.m_tickersStr = url.searchParams.get('tickers');
-    if (this.m_tickersStr != null) // If there are no tickers in the URL, do not process the request to obtain getPctChnData.
+    if (this.m_tickersStr != null && this.m_tickersStr.length != 0) // If there are no tickers in the URL, do not process the request to obtain getPctChnData.
       this.getPctChnData(this.m_tickersStr);
   }
 
@@ -73,6 +55,25 @@ export class AppComponent {
     const url: string = this.m_controllerBaseUrl + 'GetPctChnData';
     this.m_httpClient.post<string>(url, body).subscribe((response) => {
       console.log('percentage channel data:', response);
+      this.processAssetHistPctChnData(response);
     }, (error) => console.error(error));
+  }
+
+  processAssetHistPctChnData(assetPctChnData: any) {
+    this.m_assetHistDatas.length = 0; // empty the m_assetHistDatas
+    for (const assetData of assetPctChnData) {
+      const assetHistData = new AssetHistData();
+      assetHistData.symbol = assetData.Item1;
+      for (let i = assetData.Item2.length - 1; i >= 0; i--) { // The data is currently sorted in ascending order by date, but for display on the UI, we need the latest date at the top followed by earlier dates.
+        const pctChn = assetData.Item2[i];
+        const pctChnData = new PctChnData();
+        pctChnData.Date = new Date(pctChn.Item1);
+        pctChnData.pctChnWeightAggregate = pctChn.Item2;
+        for (let j = 0; j < 4; j++)
+          pctChnData[`pctChnSignal${j + 1}`] = pctChn.Item3[j].Item1;
+        assetHistData.pctChnDatas.push(pctChnData);
+      }
+      this.m_assetHistDatas.push(assetHistData);
+    }
   }
 }
