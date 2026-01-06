d06eb2f3cb41435ec7406b7b1960fc635ad2e9cf
gyantal <gyantal1@gmail.com>
2026-01-06 00:14:24 +0000

    Upgrading Angular13 to 14

--- src/WebServer/SqCoreWeb/PreDebugBuildDev.py
+++ src/WebServer/SqCoreWeb/PreDebugBuildDev.py
@@ -16,7 +16,7 @@ else:
     if (nodeRetCode != 0) :
         sys.exit("SqBuild: Node.js is required to build and run this project. To continue, please install Node.js from https://nodejs.org/")
     os.system("npm install")    # Install the dependencies in the local node_modules folder.
-    angularRetCode = os.system("ng --version") # requires the local node_modules folder, otherwise raises unhandled exception
+    angularRetCode = os.system("ng version") # requires the local node_modules folder, otherwise raises unhandled exception
     if (angularRetCode != 0) :
         sys.exit("SqBuild: NodeJs's AngularCLI is required to build and run this project. To continue, please install 'npm install -g @angular/cli'")
     Path(nodeTouchFile).touch()
