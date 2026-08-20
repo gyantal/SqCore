4eb25cc843d8f4284b8035e2f979babdbd48cb78
gyantal <gyantal1@gmail.com>
2026-08-20 19:51:34 +0100

    Upgrade Angular 20 to 21

--- src/WebServer/SqCoreWeb/projects/ChartGenerator/src/main.ts
+++ src/WebServer/SqCoreWeb/projects/ChartGenerator/src/main.ts
@@ -1,4 +1,4 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, provideZoneChangeDetection } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
@@ -15,5 +15,5 @@ function windowOnLoad() {
 }
 window.onload = windowOnLoad;
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic().bootstrapModule(AppModule, { applicationProviders: [provideZoneChangeDetection()], })
     .catch((err) => console.error(err));
